80e6b6e98eb1d3c11eed1ca2072d844f872a3490
mathesukkj <kemuel.g7363@gmail.com>
2024-04-12 21:38:54 -0300

    feat: start gin server in main file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 
-	"gotodo/db"
-	"gotodo/handlers"
+	"github.com/gin-gonic/gin"
+
+	"go-expense-tracker/db"
 )
 
 func main() {
+	r := gin.Default()
+
 	if err := db.Init(); err != nil {
 		log.Fatal("couldnt connect to the database!")
 	}
+
+	r.Run(":2024")
 }
