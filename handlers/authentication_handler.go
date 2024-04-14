bda2a095e4078b740516e3a1a6c025220a6eb418
mathesukkj <kemuel.g7363@gmail.com>
2024-04-14 00:32:29 -0300

    feat: add jwt cookie setting in response

--- handlers/authentication_handler.go
+++ handlers/authentication_handler.go
@@ -4,13 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 
 	"go-expense-tracker/db"
 	"go-expense-tracker/models"
 )
 
+var secretKey = []byte("my-secret-string")
+
 func Login(c *gin.Context) {
+	if _, err := c.Cookie("token"); err == nil {
+		c.Redirect(http.StatusFound, "/")
+		return
+	}
+
 	var loginData models.LoginPayload
 	if err := c.ShouldBindJSON(&loginData); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -21,9 +29,27 @@ func Login(c *gin.Context) {
 	db.Gorm.Find(&user, "email = ?", loginData.Email)
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginData.Password)); err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "passwords are not equal"})
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "wrong email or password"})
 		return
 	}
 
-	c.String(200, "fala paizaao")
+	token, err := createToken(user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "thats weird. an error happened!"})
+	}
+
+	c.SetCookie("token", token, 3600, "/", "localhost", false, true)
+
+	c.Redirect(http.StatusFound, "/")
+}
+
+func createToken(id uint) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{"id": id})
+
+	tokenStr, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
 }
